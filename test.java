275746234234a21e5f9a6fd34087b818085fcfee
4runner99 <julian7005@gmail.com>
2024-02-10 15:21:56 -0800

    this is a new branch

--- test.java
+++ test.java
@@ -1,8 +1,10 @@
 public class test {
 static String test = "hello";
 static String no = "high";
+
     public static void main(String[] args) {
         
+        System.out.println(test);
         System.out.println(test);
     }
     
